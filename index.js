dc980cdeb839d1cfc0bd2a7cfe77fa14c9cdb8d2
Adri-0311 <77983983+Adri-0311@users.noreply.github.com>
2023-11-30 12:13:50 +0100

    Added package express-fileupload and show data of the upload file in json format

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+const fileUpload = require("express-fileupload");
+var cors = require("cors");
+require("dotenv").config();
 
 var app = express();
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(fileUpload());
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
 
-
+  const { upfile } = req.files;
+  res
+    .status(200)
+    .json({ name: upfile.name, type: upfile.mimetype, size: upfile.size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log(`Your app is listening on port: http://localhost:${port} `);
 });
